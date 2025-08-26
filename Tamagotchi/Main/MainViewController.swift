dbe1c109a84a90b45a60df8cf5c5bc4c680ae92d
morgan4563 <morganpark4563@gmail.com>
2025-08-27 08:16:24 +0900

    refactor: Main MVVM분리, 캐릭터 변경시 이미지 바로 업데이트 안되는 에러 발생, 닉네임 변경시 변경 안되는 에러 발생

--- Tamagotchi/Main/MainViewController.swift
+++ Tamagotchi/Main/MainViewController.swift
@@ -19,12 +19,10 @@ class MainViewController: UIViewController {
         view = rootView
     }
 
+    //TODO: 0828 질문 3. viewWaillAppear는 Rx Input으로 어떻게 넣는가
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print(#function)
-        guard let data = UserDefaults.standard.loadTamagochi() else { return }
-        configure(data: data)
-        navigationItem.title = "\(currentData?.owner ?? "")님의 다마고치"
     }
 
     override func viewDidLoad() {
@@ -36,62 +34,51 @@ class MainViewController: UIViewController {
 
     func configure(data: TamagochiData) {
     	currentData = data
-        rootView.bubbleMessage.text = "안녕하세요" // 추후 변경필요
         rootView.nameLabel.text = data.name
         rootView.characterImage.image = UIImage(named: data.iamgeName)
-
-        updateStandartMessage()
-        updateLv()
-        updateImage()
-        updateStatusLabel()
     }
 
     private func bind() {
-        rootView.riceButton.rx.tap
-            .withLatestFrom(rootView.riceTextField.rx.text.orEmpty)
-            .bind(with: self) { owner, text in
-                guard var data = owner.currentData else { return }
-                let count: Int
-                if text.isEmpty {
-                    count = 1
-                } else if let num = Int(text), num > 0, num <= 99 {
-					count = num
-                } else {
-                    owner.rootView.bubbleMessage.text = "정확한 값을 입력해주세요!"
-                    return
-                }
-                data.rice += count
-                owner.currentData = data
-                UserDefaults.standard.saveTamagochi(owner.currentData!)
+        let input = MainViewModel.Input(
+            riceButtonTap: rootView.riceButton.rx.tap,
+            riceTextField: rootView.riceTextField.rx.text.orEmpty,
+            waterButtonTap: rootView.waterButton.rx.tap,
+            waterTextField: rootView.waterTextField.rx.text.orEmpty
+        )
 
-                owner.updateStandartMessage()
-                owner.updateLv()
-                owner.updateImage()
-                owner.updateStatusLabel()
+        let output = viewModel.transform(input: input)
+
+        output.bubbleMessage
+            .bind(to: rootView.bubbleMessage.rx.text)
+            .disposed(by: disposeBag)
+
+        output.currentData
+            .compactMap { $0 }
+            .bind(with: self) { owner, data in
+                owner.configure(data: data)
+                owner.navigationItem.title = "\(data.owner)님의 다마고치"
             }
             .disposed(by: disposeBag)
 
-        rootView.waterButton.rx.tap
-            .withLatestFrom(rootView.waterTextField.rx.text.orEmpty)
-            .bind(with: self) { owner, text in
-                guard var data = owner.currentData else { return }
-                let count: Int
-                if text.isEmpty {
-                    count = 1
-                } else if let num = Int(text), num > 0, num <= 49 {
-                    count = num
-                } else {
-                    owner.rootView.bubbleMessage.text = "정확한 값을 입력해주세요!"
-                    return
-                }
-                data.water += count
-                owner.currentData = data
-                UserDefaults.standard.saveTamagochi(owner.currentData!)
+        output.statusText
+            .bind(to: rootView.statusLabel.rx.text)
+            .disposed(by: disposeBag)
 
-                owner.updateStandartMessage()
-                owner.updateLv()
-                owner.updateImage()
-                owner.updateStatusLabel()
+        output.imageName
+            .bind(with: self) { owner, name in
+                owner.rootView.characterImage.image = UIImage(named: name)
+            }
+            .disposed(by: disposeBag)
+
+        output.titleText
+            .bind(with: self) { owner, title in
+                owner.navigationItem.title = title
+            }
+            .disposed(by: disposeBag)
+
+        output.nameText
+            .bind(with: self) { owner, name in
+                owner.rootView.nameLabel.text = name
             }
             .disposed(by: disposeBag)
 
@@ -103,44 +90,7 @@ class MainViewController: UIViewController {
             .disposed(by: disposeBag)
     }
 
-    private func updateStatusLabel() {
-        guard let data = currentData else { return }
-        rootView.statusLabel.text = "LV\(data.lv) • 밥알 \(data.rice) • 물방울 \(data.water)"
-    }
-
-    private func updateLv() {
-        guard var data = currentData else { return }
-        let lv = min(10, max(1,(Int((Double(data.rice) / 5.0) + (Double(data.water) / 2.0)) / 10)))
-        if data.lv != lv {
-            updateLvMessage()
-        }
-        data.lv = lv
-        currentData = data
-    }
-
-    private func updateImage() {
-        guard let data = currentData, data.lv <= 10 else { return }
-        let lv = min(9,data.lv)
-        let newImageString = "\(data.id)-\(lv)"
-        currentData?.iamgeName = newImageString
-        rootView.characterImage.image = UIImage(named: newImageString)
-    }
-
-    private func updateStandartMessage() {
-        guard let data = currentData else { return }
-        let messages = ["*#님, 좋은 하루에요", "*#님 밥 주세요", "좋은 하루에요 *#님"]
-        var randomMessage = messages.randomElement()
-        randomMessage?.replace("*#", with: data.owner)
-        rootView.bubbleMessage.text = randomMessage
-    }
-
-    private func updateLvMessage() {
-        guard let data = currentData else { return }
-        let message = "밥과 물을 잘먹었더니 레벨업 했어요 고마워요 \(data.owner)님"
-        rootView.bubbleMessage.text = message
-    }
-
-    func configureNavigation() {
+    private func configureNavigation() {
         navigationItem.title = "\(currentData?.owner ?? "")님의 다마고치"
         navigationItem.hidesBackButton = true
 
--- Tamagotchi/Main/MainViewModel.swift
+++ Tamagotchi/Main/MainViewModel.swift
@@ -10,22 +10,139 @@ import RxSwift
 import RxCocoa
 
 struct MainViewModel {
-//    let disposeBag = DisposeBag()
-//
-//    struct Input {
-//        let riceButtonTapped: ControlEvent<Void>
-//        let waterButtonTapped: ControlEvent<Void>
-//    }
-//
-//    struct Output {
-//        let updateData: PublishRelay<TamagochiData>
-//    }
-//
-//    init() {}
-//
-//    func transform(input: Input) -> Output {
-//		
-//
-//        return Output(updateData: )
-//    }
+    private let disposeBag = DisposeBag()
+
+    struct Input {
+        let riceButtonTap: ControlEvent<Void>
+        let riceTextField: ControlProperty<String>
+        let waterButtonTap: ControlEvent<Void>
+        let waterTextField: ControlProperty<String>
+    }
+
+    struct Output {
+        let bubbleMessage: BehaviorRelay<String>
+        let currentData: BehaviorSubject<TamagochiData?>
+        let statusText: BehaviorRelay<String>
+        let imageName: BehaviorRelay<String>
+        let titleText: BehaviorRelay<String>
+        let nameText: BehaviorRelay<String>
+    }
+
+    init() {}
+
+    func transform(input: Input) -> Output {
+        let bubbleMessage = BehaviorRelay<String>(value: "반갑습니다")
+        let currentData = BehaviorSubject<TamagochiData?>(value: UserDefaults.standard.loadTamagochi())
+        let statusText = BehaviorRelay<String>(value: "LV • 밥알 • 물방울 (정보 없음)")
+        let imageName = BehaviorRelay<String>(value: "noImage")
+        let titleText = BehaviorRelay<String>(value: "다마고치")
+        let nameText = BehaviorRelay<String>(value: "다마고치")
+
+        //MARK: 초기값 있으면 주기
+        if let data = try? currentData.value() {
+            statusText.accept("LV\(data.lv) • 밥알 \(data.rice) • 물방울 \(data.water)")
+            imageName.accept(data.iamgeName)
+            titleText.accept("\(data.owner)님의 다마고치")
+            nameText.accept(data.name)
+            bubbleMessage.accept("좋은 하루에요, \(data.owner)님")
+        }
+
+        input.riceButtonTap
+            .withLatestFrom(input.riceTextField)
+            .bind { text in
+                guard var data = try? currentData.value() else {
+                    bubbleMessage.accept("다마고치를 불러오지 못했습니다.")
+                    return
+                }
+
+                let count: Int
+                if text.isEmpty {
+                    count = 1
+                } else if let num = Int(text), num > 0, num <= 99 {
+                    count = num
+                } else {
+                    bubbleMessage.accept("정확한 값을 입력해주세요!")
+                    return
+                }
+                data.rice += count
+
+                let score = (Double(data.rice) / 5.0) + (Double(data.water) / 2.0)
+                let rawLv = Int(score / 10.0)
+                let checkingLv = max(1, min(10, rawLv))
+                if data.lv != checkingLv {
+                    let message = "밥과 물을 잘먹었더니 레벨업 했어요 고마워요 \(data.owner)님"
+                    bubbleMessage.accept(message)
+                } else {
+                    let messages = ["*#님, 좋은 하루에요", "*#님 밥 주세요", "좋은 하루에요 *#님"]
+                    var randomMessage = messages.randomElement()
+                    randomMessage?.replace("*#", with: data.owner)
+                    bubbleMessage.accept(randomMessage ?? "기분이 좋아요")
+                }
+                data.lv = checkingLv
+
+                currentData.onNext(data)
+                UserDefaults.standard.saveTamagochi(data)
+
+                statusText.accept("LV\(data.lv) • 밥알 \(data.rice) • 물방울 \(data.water)")
+                let lv = min(9,data.lv)
+                let newImageString = "\(data.id)-\(lv)"
+                imageName.accept(newImageString)
+                titleText.accept("\(data.owner)님의 다마고치")
+                nameText.accept(data.name)
+            }
+            .disposed(by: disposeBag)
+
+        input.waterButtonTap
+            .withLatestFrom(input.waterTextField)
+            .bind { text in
+                guard var data = try? currentData.value() else {
+                    bubbleMessage.accept("다마고치를 불러오지 못했습니다.")
+                    return
+                }
+                let count: Int
+                if text.isEmpty {
+                    count = 1
+                } else if let num = Int(text), num > 0, num <= 49 {
+                    count = num
+                } else {
+                    bubbleMessage.accept("정확한 값을 입력해주세요!")
+                    return
+                }
+                data.water += count
+
+                let score = (Double(data.rice) / 5.0) + (Double(data.water) / 2.0)
+                let rawLv = Int(score / 10.0)
+                let checkingLv = max(1, min(10, rawLv))
+                if data.lv != checkingLv {
+                    let message = "밥과 물을 잘먹었더니 레벨업 했어요 고마워요 \(data.owner)님"
+                    bubbleMessage.accept(message)
+                } else {
+                    let messages = ["*#님, 좋은 하루에요", "*#님 밥 주세요", "좋은 하루에요 *#님"]
+                    var randomMessage = messages.randomElement()
+                    randomMessage?.replace("*#", with: data.owner)
+                    bubbleMessage.accept(randomMessage ?? "기분이 좋아요")
+                }
+                data.lv = checkingLv
+
+                currentData.onNext(data)
+                UserDefaults.standard.saveTamagochi(data)
+
+                statusText.accept("LV\(data.lv) • 밥알 \(data.rice) • 물방울 \(data.water)")
+                let lv = min(9,data.lv)
+                let newImageString = "\(data.id)-\(lv)"
+                imageName.accept(newImageString)
+                titleText.accept("\(data.owner)님의 다마고치")
+                nameText.accept(data.name)
+            }
+            .disposed(by: disposeBag)
+
+        return Output(
+            bubbleMessage: bubbleMessage,
+            currentData: currentData,
+            statusText: statusText,
+            imageName: imageName,
+            titleText: titleText,
+            nameText: nameText
+        )
+    }
 }
